0659f086aceb9374f4672f73a4e296779e768751
Azeez-Ibrahim <azeez2910@outlook.com>
2022-06-27 11:12:08 +0000

    precision of 2 digits

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 number = 3.14159
-print(f"Float:{round(number,2)}")# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"Float: {round(number,2)}")# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
