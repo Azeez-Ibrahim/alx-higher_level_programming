1f9747761e638e5ab6bf18689436f16e12b18d6a
Azeez-Ibrahim <azeez2910@outlook.com>
2022-06-27 15:56:22 +0000

    correction

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 number = 98
-print(f"{number:d} Battery street\n")# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"{number:d} Battery street")# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
