d684ca4a3ce281ee13a2bc52d989776ac269c949
Azeez Ibrahim <azeez2910@outlook.com>
2022-06-30 17:58:37 +0000

    add task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
 
     num = len(sys.argv) - 1
     if num == 0:
-        print("{} arguments:".format(num))
+        print("{} argument:".format(num))
     elif num == 1:
-        print("{} arguments:".format(num))
+        print("{} argument:".format(num))
     else:
         print("{} arguments:".format(num))
     for i in range(1, num + 1):
