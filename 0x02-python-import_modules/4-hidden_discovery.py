3bbcba707eb9f78ca969d02b014e5b1f85c4f804
Azeez Ibrahim <azeez2910@outlook.com>
2022-06-30 19:02:22 +0000

    correction to task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from hidden_4 import *
 
-    listmodule = dir()
-    for i in listmodule:
-        if listmodule[i][0:2] != "__":
-            print("{}".format(listmodule[i]))
+    arr = dir()
+    for i in range(0, len(arr)):
+        if arr[i][0:2] != "__":
+            print("{}".format(arr[i]))
