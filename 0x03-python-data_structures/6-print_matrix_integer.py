4665ac00777455a15b6588ff664fba36084fe7e6
Azeez Ibrahim <azeez2910@outlook.com>
2022-07-03 19:17:28 +0000

    add task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
+
+
 def print_matrix_integer(matrix=[[]]):
-    if matrix == [[]]:
-        print()
-    else:
-        for i in range(0, len(matrix)):
-            if len(matrix[i]) == 0:
-                print()
-            for j in range(0, len(matrix)):
-                if j != len(matrix) - 1:
-                    print("{:d}".format(matrix[i][j]), end=" ")
-                else:
-                    print("{:d}".format(matrix[i][j]))
+    for i in range(0, len(matrix)):
+        if len(matrix[i]) == 0:
+            print()
+        for j in range(0, len(matrix[i])):
+            if j != len(matrix[i]) - 1:
+                print("{:d}".format(matrix[i][j]), end=" ")
+            elif j == len(matrix[i]) - 1:
+                print("{:d}".format(matrix[i][j]))
