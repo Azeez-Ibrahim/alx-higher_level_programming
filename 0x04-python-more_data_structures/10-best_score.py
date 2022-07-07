29f0dbb4997076a46236e2b91fab24d34e3c32bc
Azeez Ibrahim <azeez2910@outlook.com>
2022-07-07 10:24:45 +0000

    correction to task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    if a_dictionary = {}:
+    if a_dictionary == {}:
         return None
     keyList = list(a_dictionary.keys())
     valList = list(a_dictionary.values())
