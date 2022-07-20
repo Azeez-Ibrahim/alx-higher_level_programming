cc5339a1db471a9a536a7fdc2b05b45e0f22c1a4
Azeez Ibrahim <azeez2910@outlook.com>
2022-07-20 20:33:42 +0000

    task 100

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """No module added"""
 
+
 class Node:
     """Node of a singly linked list.
     Private instance attribute: data:
@@ -43,7 +44,7 @@ class Node:
 
 
 class SinglyLinkedList:
-    """ Singly linked list.
+    """Singly linked list.
     Private instance attribute: head.
     Simple instantiation.
     Public instance method: def sorted_insert(self, value).
@@ -59,7 +60,7 @@ class SinglyLinkedList:
         node = self.head
         while node:
             my_str += str(node.data)
-            my_str += '\n'
+            my_str += "\n"
             node = node.next_node
         return my_str[:-1]
 
