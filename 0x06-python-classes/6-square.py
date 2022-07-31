dfd12e6457163fb9e1473cd1b1d63b51f3f64abc
Azeez Ibrahim <azeez2910@outlook.com>
2022-07-31 20:57:55 +0000

    test

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -61,8 +61,6 @@ class Square:
         if self.__size == 0:
             print()
             return
-        for y in range(0, self.__position[1]):
-            print()
         for i in range(0, self.__size):
             for x in range(0, self.__position[0]):
                 print(" ", end="")
