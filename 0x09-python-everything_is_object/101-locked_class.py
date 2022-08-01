d64bfb47ecf09fe6d0657e8590dad2bc3f0bdeec
Azeez Ibrahim <azeez2910@outlook.com>
2022-08-01 14:02:58 +0000

    add task 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""No modules imported"""
+
+
 class LockedClass:
+    """Class LockedClass"""
 
-    __slots__ = ['first_name']
+    __slots__ = ["first_name"]
 
-    def __init__(self, first_name=''):
+    def __init__(self, first_name=""):
+        """Initialize LockedClass instance"""
         self.first_name = first_name
